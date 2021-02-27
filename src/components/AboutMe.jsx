435871f0bf7d92a6272935b53e76d7ddcff4e2fc
Lucas Ferreira <lucasfsilva2310gmail.com>
2021-02-27 18:58:40 -0300

    Component AboutMe Finished and styled

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,18 @@
 import {
   AboutDivSection,
   CodeContainer,
+  CodeLineNumber,
   CodeRow,
+  Comment,
   Declaration,
+  Emphasis,
   Names,
   Signs,
+  Space,
+  TickingSpan,
+  VarName,
+  VarValueNumber,
+  VarValues,
 } from "../styles/AboutDivSection";
 
 const AboutMe = () => {
@@ -14,8 +22,296 @@ const AboutMe = () => {
       <CodeContainer>
         <CodeRow>
           <div>
-            <Declaration>class </Declaration> <Names>Lucas</Names>{" "}
+            <CodeLineNumber>01</CodeLineNumber> <Space />
+            <Space />
+            <Declaration>class </Declaration>
+            <Space /> <Names>Lucas</Names> <Space />
             <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>02</CodeLineNumber> <Space /> <Space />
+            <Space />
+            <Space />
+            <Comment>{`//`} Empolgado em aprender novas tecnologias.</Comment>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>03</CodeLineNumber> <Space /> <Space />
+            <Space />
+            <Space />
+            <Comment>
+              {`//`} Minha quantidade e qualidade hard e soft skills aumenta
+              diariamente {`:)`}
+            </Comment>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>04</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Declaration>developer</Declaration>
+            <Signs>{`()`}</Signs> <Space /> <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>05</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Emphasis>this</Emphasis>
+            <Signs>.</Signs>
+            <VarName>name</VarName>
+            <Space />
+            <Emphasis>=</Emphasis>
+            <Space />
+            <VarValues>'Lucas Ferreira Silva'</VarValues>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>06</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Emphasis>this</Emphasis>
+            <Signs>.</Signs>
+            <VarName>age</VarName>
+            <Space />
+            <Emphasis>=</Emphasis>
+            <Space />
+            <VarValueNumber>26</VarValueNumber>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>07</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Emphasis>this</Emphasis>
+            <Signs>.</Signs>
+            <VarName>email</VarName>
+            <Space />
+            <Emphasis>=</Emphasis>
+            <Space />
+            <VarValues>'lucasfsilva2310@gmail.com'</VarValues>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>08</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>09</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Names>workExperience</Names>
+            <Signs>{`()`}</Signs> <Space /> <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>10</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Declaration>return</Declaration> <Space />
+            <Signs>{`[`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>11</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />{" "}
+            <VarValues>'08/2019 - 06/2020'</VarValues> <Space />{" "}
+            <Signs>:</Signs> <Space />{" "}
+            <VarValues>
+              'Aram Bistrô Café e Bar - Gerente Operacional'
+            </VarValues>{" "}
+            <Space /> <Signs>{`},`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>12</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />{" "}
+            <VarValues>'04/2019 - 06/2019'</VarValues> <Space />{" "}
+            <Signs>:</Signs> <Space />{" "}
+            <VarValues>'Birds From Amazon - Assistente pessoal'</VarValues>{" "}
+            <Space /> <Signs>{`},`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>13</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />{" "}
+            <VarValues>'09/2017 - 02/2018'</VarValues> <Space />{" "}
+            <Signs>:</Signs> <Space />{" "}
+            <VarValues>'Martin Dog - Promotor de vendas'</VarValues> <Space />{" "}
+            <Signs>{`},`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>14</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />{" "}
+            <VarValues>'06/2017 - 10/2017'</VarValues> <Space />{" "}
+            <Signs>:</Signs> <Space />{" "}
+            <VarValues>'Pet Care - Auxiliar veterinário'</VarValues> <Space />{" "}
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>15</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Signs>{`]`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>16</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>17</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Names>education</Names>
+            <Signs>{`()`}</Signs>
+            <Space /> <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>18</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Declaration>return</Declaration> <Space />
+            <Signs>{`[`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>19</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space /> <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />{" "}
+            <VarValues>'07/2020 - now'</VarValues> <Space /> <Signs>:</Signs>{" "}
+            <Space />{" "}
+            <VarValues>
+              'Kenzie Academy Brasil - Desenvolvedor Web FullStack'
+            </VarValues>{" "}
+            <Space /> <Signs>{`},`}</Signs>
+          </div>
+        </CodeRow>
+
+        <CodeRow>
+          <div>
+            <CodeLineNumber>20</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Signs>{`]`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>21</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>22</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Names>exchange</Names>
+            <Signs>{`()`}</Signs> <Space /> <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>23</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Declaration>return</Declaration> <Space />
+            <Signs>{`[`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>24</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space /> <Space /> <Space />
+            <Signs>{`{`}</Signs> <Space />
+            <VarValues>'12/2018 - 07-2019'</VarValues> <Space />{" "}
+            <Signs>:</Signs> <Space />{" "}
+            <VarValues>' Australia - General English Course '</VarValues>{" "}
+            <Space /> <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>25</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Signs>{`]`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>26</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>27</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Names>skills</Names> <Signs>{`()`}</Signs> <Space />{" "}
+            <Signs>{`{`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>28</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space /> <Space /> <Space />
+            <Declaration>return</Declaration> <Space />
+            <Signs>{`[`}</Signs> <Space /> <VarValues>'HTML'</VarValues>
+            <Signs>,</Signs>
+            <Space /> <VarValues>'CSS'</VarValues>
+            <Signs>,</Signs>
+            <Space /> <VarValues>'Javascript'</VarValues>
+            <Signs>,</Signs> <Space /> <VarValues>'ReactJS'</VarValues>
+            <Signs>,</Signs>
+            <Space /> <VarValues>'Redux'</VarValues>
+            <Signs>,</Signs>
+            <Space /> <VarValues>'Python'</VarValues>
+            <Signs>,</Signs> <Space /> <VarValues>'GIT'</VarValues>
+            <Signs>,</Signs> <Space /> <VarValues>'npm/yarn'</VarValues>
+            <Signs>,</Signs> <Space /> <VarValues>'Flask'</VarValues>
+            <Signs>,</Signs> <Space /> <VarValues>'Docker'</VarValues> <Space />
+            <Signs>{`]`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>29</CodeLineNumber> <Space /> <Space />
+            <Space /> <Space />
+            <Signs>{`}`}</Signs>
+          </div>
+        </CodeRow>
+        <CodeRow>
+          <div>
+            <CodeLineNumber>30</CodeLineNumber> <Space /> <Space />
+            <Signs>{`}`}</Signs> <Space />
+            <TickingSpan>
+              <span>|</span>
+            </TickingSpan>
           </div>
         </CodeRow>
       </CodeContainer>
--- src/styles/AboutDivSection.js
+++ src/styles/AboutDivSection.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const AboutDivSection = styled.div`
+  /* border: 2px solid red; */
   background: #1e1e1f;
   width: 100%;
-  height: 62vh;
+  height: 62%;
   margin: 0 auto;
+  padding-bottom: 2%;
   color: white;
   font-size: 1.4rem;
   p {
@@ -23,7 +25,7 @@ export const CodeContainer = styled.div`
 `;
 
 export const CodeRow = styled.div`
-  border: 1px solid red;
+  /* border: 1px solid red; */
   width: 100%;
   margin-left: 2%;
   height: 1.7rem;
@@ -43,17 +45,67 @@ export const CodeRow = styled.div`
   }
 `;
 
+export const CodeLineNumber = styled.span`
+  color: white;
+  /* border-right: 1px solid grey; */
+  /* padding-right: 1%; */
+`;
+
 export const Declaration = styled.span`
-  margin-left: 0.7%;
-  color: rgb(34, 34, 236);
+  color: rgb(51, 126, 177);
+  font-style: italic;
+  font-weight: 400;
 `;
 
 export const Names = styled.span`
-  margin-left: 0.7%;
-  color: yellow;
+  color: rgb(236, 224, 52);
+  font-weight: 800;
 `;
 
 export const Signs = styled.span`
+  color: rgb(207, 204, 204);
+`;
+
+export const Space = styled.span`
   margin-left: 0.7%;
-  color: white;
+`;
+
+export const Comment = styled.span`
+  color: grey;
+`;
+
+export const Emphasis = styled.span`
+  color: rgb(196, 33, 33);
+  font-weight: 500;
+`;
+
+export const VarName = styled.span`
+  color: rgb(118, 250, 118);
+`;
+
+export const VarValues = styled.span`
+  color: rgb(41, 160, 41);
+  font-weight: 600;
+`;
+
+export const VarValueNumber = styled.span`
+  color: orange;
+`;
+
+export const TickingSpan = styled.span`
+  span {
+    color: white;
+    animation: ticking;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes ticking {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
 `;
