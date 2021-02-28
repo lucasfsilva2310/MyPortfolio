6e7ea2090fa62464336d398497c6b7834eab6e27
Lucas Ferreira <lucasfsilva2310gmail.com>
2021-02-28 14:46:47 -0300

    Projects component structured added.

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,11 +1,60 @@
-import { ProjectsDivSection } from "../styles/ProjectsDivSection";
+import {
+  ProjectsContainer,
+  ProjectsDivSection,
+} from "../styles/ProjectsDivSection";
+
+import { Box } from "grommet";
 
 const MyProjects = () => {
   return (
     <ProjectsDivSection>
       <p id="projects">{`<!..Projetos..>`}</p>
+      <ProjectsContainer>
+        {Projects.map((project) => {
+          return (
+            <Box
+              animation={project.animation}
+              align="center"
+              border={{ style: "solid", size: "medium" }}
+              height="small"
+              margin={{ vertical: "xlarge", horizontal: "medium" }}
+              focusIndicator={true}
+              onClick={() => console.log("oi")}
+            >
+              <img src={project.image} alt={project.title} />
+              <span>{project.title}</span>
+            </Box>
+          );
+        })}
+      </ProjectsContainer>
     </ProjectsDivSection>
   );
 };
 
 export default MyProjects;
+
+const Projects = [
+  {
+    title: "MyOwnPokédex",
+    description: "Descrição",
+    technologies: "tecnologias",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXHdOdw4iIpCNMLDNGxxnB4L47gGLWAA5nig&usqp=CAU",
+    animation: { type: "pulse", duration: 4000 },
+  },
+  {
+    title: "Dbookshelf",
+    description: "Descrição",
+    technologies: "tecnologias",
+    image: "https://mdn.mozillademos.org/files/8531/web-site.jpg",
+    animation: { type: "jiggle", duration: 4000 },
+  },
+  {
+    title: "PomoGenius",
+    description: "Descrição",
+    technologies: "tecnologias",
+    image:
+      "https://media.gcflearnfree.org/content/5e9745ab65c51443f80a55e8_04_15_2020/exploring.png",
+    animation: { type: "pulse", duration: 4000 },
+  },
+];
--- src/styles/ProjectsDivSection.js
+++ src/styles/ProjectsDivSection.js
@@ -3,12 +3,29 @@ import styled from "styled-components";
 export const ProjectsDivSection = styled.div`
   background: radial-gradient(#161616, #38353c);
   width: 100%;
-  height: 62vh;
+  height: 62%;
   color: white;
   font-size: 1.4rem;
   p {
     margin-left: 20%;
     margin-top: 2%;
     opacity: 0.7;
+  }
+`;
+
+export const ProjectsContainer = styled.div`
+  width: 90%;
+  height: 90%;
+  margin: 2% auto;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  justify-content: space-around;
+
+  img {
+    width: 325px;
+    height: auto;
   }
 `;
