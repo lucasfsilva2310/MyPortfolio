204288bb1dc72fea2dfde9fb186068e99aa8b604
lucas <lucas.f.silva@provi.com.br>
2022-07-26 20:04:17 -0300

    changing Header text content

--- src/components/HeadContent.jsx
+++ src/components/HeadContent.jsx
@@ -7,7 +7,7 @@ const HeadContent = () => {
       <TextBox>
         <p>Hello.</p>
         <p>My name is Lucas Ferreira Silva</p>
-          <p>and I develop websites</p>
+          <p>and I am a FullStack developer</p>
         <CheckIt>Come have a look!</CheckIt>
       </TextBox>
     </MainDivSection>
